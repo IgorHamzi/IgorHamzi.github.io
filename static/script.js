f23ec24efccb04e69d6c7cf4d0a98478ff1e8d87
IgorHamzi <igorwhamzi@gmail.com>
2023-01-24 16:14:21 -0300

    feat: add animation

--- static/script.js
+++ static/script.js
@@ -1,22 +1,40 @@
-const menuMobile = document.querySelector('.menu-mobile')
-const body = document.querySelector('body')
+const menuMobile = document.querySelector('.menu-mobile');
+const body = document.querySelector('body');
 
 menuMobile.addEventListener('click', ()=>{
   if(menuMobile.classList.contains("bi-list")){
-    menuMobile.classList.replace("bi-list", "bi-x")
+    menuMobile.classList.replace("bi-list", "bi-x");
   } else{
-    menuMobile.classList.replace("bi-x", "bi-list")
+    menuMobile.classList.replace("bi-x", "bi-list");
   };
-  body.classList.toggle("menu-nav-active")
-})
+  body.classList.toggle("menu-nav-active");
+});
 
-const navItem = document.querySelectorAll('.nav-item')
+const navItem = document.querySelectorAll('.nav-item');
 
 navItem.forEach(item => {
   item.addEventListener("click", () => {
     if(body.classList.contains("menu-nav-active")) {
       body.classList.remove("menu-nav-active")
-      menuMobile.classList.replace("bi-x", "bi-list")
+      menuMobile.classList.replace("bi-x", "bi-list");
     }
-  })
+  });
+});
+
+const item = document.querySelectorAll("[data-anime]");
+
+const animeScroll = () => {
+  const windowTop = window.pageYOffset + window.innerHeight * 0.85;
+
+  item.forEach(element => {
+    if (windowTop > element.offsetTop) {
+      element.classList.add('animate')
+    } else {
+      element.classList.remove('animate')
+    }
+  });
+};
+
+window.addEventListener("scroll", () => {
+  animeScroll();
 })
